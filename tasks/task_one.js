935e584fb5b0ef1238e2d028ee824f2f81d8e072
Arinze Atusiuba <arinzeatusiuba@gmail.com>
2023-07-14 15:51:26 +0100

    Created and manipulated strings

--- tasks/task_one.js
+++ tasks/task_one.js
@@ -1,2 +1,19 @@
+const name = 'Arinze'
+let result = name
+console.log(result) // Output: "Arinze"
 
-// Write something
+const fullName = 'Arinze ' + 'Atusiuba'
+result = fullName
+console.log(result) // Output: 'Arinze Atusiuba'
+
+const uppercaseFullName = fullName.toUpperCase()
+result = uppercaseFullName
+console.log(result) // Output: "ARINZE ATUSIUBA"
+
+const lowercaseFullName = fullName.toLowerCase()
+result = lowercaseFullName
+console.log(result) // Output: "arinze atusiuba"
+
+const slicedFullName = fullName.slice(7, 16)
+result = slicedFullName
+console.log(result) // Output: "Atusiuba"
